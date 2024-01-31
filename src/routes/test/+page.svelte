f376c7f3d986eb3509792d29189ce4224515c484
Vojtech Simetka <vojtech@simetka.cz>
2024-01-31 20:40:47 +0100

    feat: add test page for different languages

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import dic from 'dictionary-de/index.dic?raw'
+	type Count = {
+		[key: string | number]: number
+	}
+
+	const minLength = 3
+	const maxLength = 19
+	const words = dic
+		.split('\n')
+		.filter((item) => !/^(\s|\p{Lu}|\p{N})/u.test(item))
+		.map((item) => item.replace(/\/.*$/, ''))
+		.filter((item) => item.length >= minLength && item.length <= maxLength)
+	const lengths: Count = {}
+	words.forEach((item) => {
+		const len = item.length
+		if (!lengths[len]) lengths[len] = 0
+		lengths[len]++
+	})
+
+	// Find the longest word
+	let longest = ''
+	words.forEach((item) => {
+		if (item.length > longest.length) longest = item
+	})
+</script>
+
+<div>
+	<p>Nejdelsi slovo: {longest} ({longest.length} znaku)</p>
+	<p>Celkovy pocet slov {words.length}</p>
+	Pocet slov podle delky:
+	{#each Object.keys(lengths) as len}
+		<div>- {len}: {lengths[len]}</div>
+	{/each}
+	<!-- {#each words.filter(s => s.length === 9) as w}
+        <div>{w}</div>
+    {/each} -->
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
+import path from 'path'
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'dictionary-en': path.resolve(__dirname, 'node_modules/dictionary-en'),
+			'dictionary-cs': path.resolve(__dirname, 'node_modules/dictionary-cs'),
+			'dictionary-de': path.resolve(__dirname, 'node_modules/dictionary-de'),
+		},
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
